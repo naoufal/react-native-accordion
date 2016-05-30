1d8b6d448889612e60f2c73bb6ca2e1f78612cc8
Andrey Petrash <andrey@ekreative.com>
2016-05-30 10:21:32 +0300

    update for react-native 0.26

--- src/index.js
+++ src/index.js
@@ -1,124 +1,118 @@
 'use strict';
 
-var React = require('react-native');
+var React = require('react');
+var {TouchableHighlight, View} = require('react-native');
 var tweenState = require('react-tween-state');
 
-var {
-  StyleSheet,
-  TouchableHighlight,
-  View,
-  Text
-} = React;
-
 var Accordion = React.createClass({
-  mixins: [tweenState.Mixin],
+    mixins: [tweenState.Mixin],
 
-  propTypes: {
-    activeOpacity: React.PropTypes.number,
-    animationDuration: React.PropTypes.number,
-    content: React.PropTypes.element.isRequired,
-    easing: React.PropTypes.string,
-    expanded: React.PropTypes.bool,
-    header: React.PropTypes.element.isRequired,
-    onPress: React.PropTypes.func,
-    underlayColor: React.PropTypes.string,
-    style: React.PropTypes.object
-  },
+    propTypes: {
+        activeOpacity: React.PropTypes.number,
+        animationDuration: React.PropTypes.number,
+        content: React.PropTypes.element.isRequired,
+        easing: React.PropTypes.string,
+        expanded: React.PropTypes.bool,
+        header: React.PropTypes.element.isRequired,
+        onPress: React.PropTypes.func,
+        underlayColor: React.PropTypes.string,
+        style: React.PropTypes.object
+    },
 
-  getDefaultProps() {
-    return {
-      activeOpacity: 1,
-      animationDuration: 300,
-      easing: 'linear',
-      expanded: false,
-      underlayColor: '#000',
-      style: {}
-    };
-  },
+    getDefaultProps() {
+        return {
+            activeOpacity: 1,
+            animationDuration: 300,
+            easing: 'linear',
+            expanded: false,
+            underlayColor: '#000',
+            style: {}
+        };
+    },
 
-  getInitialState() {
-    return {
-      is_visible: false,
-      height: 0,
-      content_height: 0
-    };
-  },
+    getInitialState() {
+        return {
+            is_visible: false,
+            height: 0,
+            content_height: 0
+        };
+    },
 
-  close() {
-    this.state.is_visible && this.toggle();
-  },
+    close() {
+        this.state.is_visible && this.toggle();
+    },
 
-  open() {
-    !this.state.is_visible && this.toggle();
-  },
+    open() {
+        !this.state.is_visible && this.toggle();
+    },
 
-  toggle() {
-    this.state.is_visible = !this.state.is_visible;
+    toggle() {
+        this.state.is_visible = !this.state.is_visible;
 
-    this.tweenState('height', {
-      easing: tweenState.easingTypes[this.props.easing],
-      duration: this.props.animationDuration,
-      endValue: this.state.height === 0 ? this.state.content_height : 0
-    });
-  },
-
-  _onPress() {
-    this.toggle();
-
-    if (this.props.onPress) {
-      this.props.onPress.call(this);
-    }
-  },
-
-  _getContentHeight() {
-    if (this.refs.AccordionContent) {
-      this.refs.AccordionContent.measure((ox, oy, width, height, px, py) => {
-        // Sets content height in state
-        this.setState({
-          height: this.props.expanded ? height : 0,
-          content_height: height
+        this.tweenState('height', {
+            easing: tweenState.easingTypes[this.props.easing],
+            duration: this.props.animationDuration,
+            endValue: this.state.height === 0 ? this.state.content_height : 0
         });
-      });
-    }
-  },
+    },
 
-  componentDidMount() {
-    // Gets content height when component mounts
-    // without setTimeout, measure returns 0 for every value.
-    // See https://github.com/facebook/react-native/issues/953
-    setTimeout(this._getContentHeight);
-  },
+    _onPress() {
+        this.toggle();
 
-  render() {
-    return (
-      /*jshint ignore:start */
-      <View
-        style={{
+        if (this.props.onPress) {
+            this.props.onPress.call(this);
+        }
+    },
+
+    _getContentHeight() {
+        if (this.refs.AccordionContent) {
+            this.refs.AccordionContent.measure((ox, oy, width, height, px, py) => {
+                // Sets content height in state
+                this.setState({
+                    height: this.props.expanded ? height : 0,
+                    content_height: height
+                });
+            });
+        }
+    },
+
+    componentDidMount() {
+        // Gets content height when component mounts
+        // without setTimeout, measure returns 0 for every value.
+        // See https://github.com/facebook/react-native/issues/953
+        setTimeout(this._getContentHeight);
+    },
+
+    render() {
+        return (
+            /*jshint ignore:start */
+            <View
+                style={{
           overflow: 'hidden'
         }}
-      >
-        <TouchableHighlight
-          ref="AccordionHeader"
-          onPress={this._onPress}
-          underlayColor={this.props.underlayColor}
-          style={this.props.style}
-        >
-          {this.props.header}
-        </TouchableHighlight>
-        <View
-          ref="AccordionContentWrapper"
-          style={{
+            >
+                <TouchableHighlight
+                    ref="AccordionHeader"
+                    onPress={this._onPress}
+                    underlayColor={this.props.underlayColor}
+                    style={this.props.style}
+                >
+                    {this.props.header}
+                </TouchableHighlight>
+                <View
+                    ref="AccordionContentWrapper"
+                    style={{
             height: this.getTweeningValue('height')
           }}
-        >
-          <View ref="AccordionContent">
-            {this.props.content}
-          </View>
-        </View>
-      </View>
-      /*jshint ignore:end */
-    );
-  }
+                >
+                    <View ref="AccordionContent">
+                        {this.props.content}
+                    </View>
+                </View>
+            </View>
+            /*jshint ignore:end */
+        );
+    }
 });
 
 module.exports = Accordion;
