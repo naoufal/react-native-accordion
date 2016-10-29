94d82f5c3351bb9e020dc24eaf22ddd49883a385
Deray <deraygo@gmail.com>
2016-10-29 09:15:09 +0800

    add _getContentHeight to componentWillReceiveProps

--- src/index.js
+++ src/index.js
@@ -82,6 +82,10 @@ var Accordion = React.createClass({
     }
   },
 
+  componentWillReceiveProps(nextProps) {
+    setTimeout(this._getContentHeight);
+  },
+
   componentDidMount() {
     // Gets content height when component mounts
     // without setTimeout, measure returns 0 for every value.
